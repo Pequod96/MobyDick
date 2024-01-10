49a84b6e4319b0f2b93dd2d086b5ffbf4c598742
Samtoro <105719435+Pequod96@users.noreply.github.com>
2024-01-10 11:49:56 +0100

    v.5

---
+++ src/index.js
@@ -0,0 +1,45 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Il tuo codice qui
+window.addEventListener("scroll", function () {
+  var scrollingDiv = document.getElementById("scrollingDiv");
+
+  // Calcola la nuova posizione in base allo scroll
+  var newPosition = -200 + window.scrollY;
+
+  // Applica la nuova posizione al div
+  scrollingDiv.style.right = newPosition + "px";
+});
+
+window.addEventListener("scroll", function () {
+  var scrollingDiv2 = document.getElementById("scrollingDiv-2");
+
+  // Controlla se la finestra è stata scrollata di almeno 300 pixel
+  if (window.scrollY > 1400) {
+    // Mostra il div
+    scrollingDiv2.style.display = "block";
+
+    // Calcola la nuova posizione in base allo scroll
+    var newPosition = -200 + (window.scrollY - 1400);
+
+    // Applica la nuova posizione al div
+    scrollingDiv2.style.left = newPosition + "px";
+  } else {
+    // Nascondi il div se non hai ancora raggiunto il punto di scorrimento desiderato
+    scrollingDiv2.style.display = "none";
+  }
+});
+
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".menu-wrap .menu a");
+  const checkbox = document.querySelector(".menu-wrap .toggler");
+
+  links.forEach((link) => {
+    link.addEventListener("click", function () {
+      // Chiudi l'hamburger menu impostando 'checked' a false
+      checkbox.checked = false;
+    });
+  });
+});
